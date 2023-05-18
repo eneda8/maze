68ff9af8906c91498ae58dea57b8b2ce1cbcbe80
Eneda Xhambazi <enedaxhambazi@gmail.com>
2023-05-17 21:35:47 -0400

    styled button, added favicon + window resize event handler

--- index.js
+++ index.js
@@ -57,14 +57,6 @@ const shuffle = arr => {
   }
   return arr;
 };
-// const grid = [];
-
-// for(let i = 0; i < 3; i++){
-//   grid.push([])
-//   for (let j = 0; j <3 ; j++){
-//     grid[i].push(false);
-//   }
-// };
 
 const grid = Array(cellsVertical)
   .fill(null)
